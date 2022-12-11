fc645400cd7fe203e3c14546420f01f32b228283
Thanh Le <thanhle01101995@outlook.com>
2022-12-10 22:12:07 -0500

    Add description

--- OOP_design_practice/Vending_Machine.py
+++ OOP_design_practice/Vending_Machine.py
@@ -1,5 +1,13 @@
 """
 Design Vending machine
+
+You need to design a Vending Machine which
+Accepts coins of 1,5,10,25 Cents i.e. penny, nickel, dime, and quarter.
+Allow user to select products Coke(25), Pepsi(35), Soda(45)
+Allow user to take refund by canceling the request.
+Return the selected product and remaining change if any
+Allow reset operation for vending machine supplier.
+
 """
 
 from enum import Enum
